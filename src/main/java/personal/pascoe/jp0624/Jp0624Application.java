1dc0c3db5cfc3ed535a253e18db6918597ee693d
jpassgo <jeffpascoe9@gmail.com>
2024-06-19 20:06:55 -0500

    Entry point for spring app

---
+++ src/main/java/personal/pascoe/jp0624/Jp0624Application.java
@@ -0,0 +1,13 @@
+package personal.pascoe.jp0624;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jp0624Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jp0624Application.class, args);
+	}
+
+}
